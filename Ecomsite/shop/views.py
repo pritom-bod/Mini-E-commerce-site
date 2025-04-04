b6c1cc4d16502d1240f2d3b491e8afde72b15215
pritom-bod <pritomsarker406@gmail.com>
2025-04-05 02:59:29 +0600

    project2 first add

---
+++ Ecomsite/shop/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Products
+
+# Create your views here.
+
+def index(request):
+    product_object = Products.objects.all()
+
+    item_name = request.GET.get('item_name')
+    if item_name != "" and item_name is not None:
+        product_object = product_object.filter(title__icontains =item_name) 
+    return render(request, 'shop/index.html', {'product_object':product_object})
