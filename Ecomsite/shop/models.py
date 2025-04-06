b1078eb371a5b8232f37d902b25ecfe91afcd2de
pritom-bod <pritomsarker406@gmail.com>
2025-04-07 01:29:31 +0600

    full upload 1st

--- Ecomsite/shop/models.py
+++ Ecomsite/shop/models.py
@@ -10,5 +10,12 @@ class Products(models.Model):
     image = models.CharField(max_length=300)
 
 
-
-    
+class order(models.Model):
+    item = models.CharField(max_length=1000)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=300)
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=50)
+    total = models.CharField(max_length=100)
--- Ecomsite/shop/views.py
+++ Ecomsite/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Products
+from .models import Products, order
 from django.core.paginator import Paginator
+
+
 # Create your views here.
 
 def index(request):
@@ -23,3 +25,21 @@ def index(request):
 def detail(request, id):
     product_objects = Products.objects.get(id=id)
     return render(request, "shop/detail.html", {'product_objects':product_objects})
+
+
+def checkout(request):
+
+    if request.method == "POST":
+        item = request.POST.get('item')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zipcode = request.POST.get('zipcode')
+        total = request.POST.get('total')
+
+        Order = order(total=total, item=item, name=name, email=email, address=address,  city=city, state=state, zipcode=zipcode)
+        Order.save()
+        
+    return render(request, 'shop/checkout.html')
